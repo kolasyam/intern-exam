0b5caea6b48a99118836308e943c2b6c66849b47
kolasyam <syammanikantakola426@gmail.com>
2024-12-06 15:45:19 +0530

    logo changes

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { FaBars } from "react-icons/fa";
 import { AiOutlineHome, AiOutlineSetting } from "react-icons/ai";
 import { FiUsers } from "react-icons/fi";
 import Image from "next/image";
